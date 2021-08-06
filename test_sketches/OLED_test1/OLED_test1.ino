948403502866f83be0cf7fb849eb5c0627163ca8
Luke Miller <millerlp@gmail.com>
2021-08-05 21:59:29 -0700

    Tested working with 128x32 I2C display
    Purchased from Amazon: "WayinTop 3PCS 0.91 inch OLED Display Module IIC"

---
+++ test_sketches/OLED_test1/OLED_test1.ino
@@ -0,0 +1,32 @@
+
+#include <Wire.h>
+#include "SSD1306Ascii.h" // https://github.com/greiman/SSD1306Ascii
+#include "SSD1306AsciiWire.h" // https://github.com/greiman/SSD1306Ascii
+
+
+SSD1306AsciiWire oled; // create OLED display object, using I2C Wire1 port
+#define I2C_ADDRESS1 0x3C // for OLED. DIY mall units list 0x78 as address, but need 0x3C to work here
+
+
+void setup() {
+  Wire.begin();
+  Wire.setClock(400000L);
+  oled.begin(&Adafruit128x32, I2C_ADDRESS1);  
+  oled.setFont(Adafruit5x7);    
+  oled.clear(); 
+  oled.home();
+//  oled.set2X();
+  oled.print("Hello");
+  oled.println();
+  oled.println(F("Did this work?"));
+  oled.println(F("Third line"));
+  oled.println(F("Fourth line"));
+  delay(1000);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
